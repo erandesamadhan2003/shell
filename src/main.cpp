3a4179b20d3eb3f5620374ebdb20e6e87bde9abf
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-05-31 18:46:27 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,19 @@ int main() {
 
   while (true) {
     std::cout << "$ ";
-    
+
     std::string input;
     getline(std::cin, input);
 
-    if(input == "exit 0") return 0;
+    if (input == "exit 0") return 0;
     
-    std::cout << input << ": command not found" << std::endl;
+    if (input.find("echo") != std::string::npos) {
+      std::string echo = "echo ";
+      std::cout << input.substr(echo.length()) << std::endl;
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    }
+
   }
 
   return 0;
