053df1df95085ca0b028618f524382158b2db2b2
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-05-31 18:06:45 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ int main() {
     
     std::string input;
     getline(std::cin, input);
+
+    if(input == "exit 0") return 0;
     
     std::cout << input << ": command not found" << std::endl;
   }
